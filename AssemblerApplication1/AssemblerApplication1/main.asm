9b30153ddfd116c56fb34d85883e247eaa32acd0
fluxtheory <fluxtheory@hotmail.com>
2017-05-31 23:51:41 +1000

    Update main.asm

--- AssemblerApplication1/AssemblerApplication1/main.asm
+++ AssemblerApplication1/AssemblerApplication1/main.asm
@@ -1056,6 +1056,7 @@ adminModeInitial:
 	ldi ZL, low(item1)   //z holds pointer to item1's inventory.
 	ldi ZH, high(item1)
 	ld temp1, Z
+	subi temp1, -'0'
 	do_lcd_data_r temp1 
 	out PORTC, temp1    //since item 1 will initially have only 1 item, no conversion is needed
 	
@@ -1064,14 +1065,146 @@ adminModeInitial:
 	ldi ZL, low(item1Cost)
 	ldi ZH, high(item1Cost)
 	ld temp1, Z
+	subi temp1, -'0'
 	do_lcd_data_r temp1 
 	pop temp1
 	rjmp adminMode
 
 
-adminMode:
+returnInventory:
+	 push temp1
+	subi temp1, '0'
+	cpi temp1, 1
+	breq return1
+	cpi temp1, 2
+	breq return2
+	cpi temp1, 3
+	breq return3
+	cpi temp1, 4
+	breq return4
+	cpi temp1, 5
+	breq return5
+	cpi temp1, 6
+	breq return6
+	cpi temp1, 7
+	breq return7
+	cpi temp1, 8
+	breq return8
+	cpi temp1, 9
+	breq return9
 
-	jmp KeypadLoop
+	return1:
+		ldi ZL, low(item1)    //inventory amount
+		ldi ZH, high(item1)
+		ldi YL, low(item1Cost) 	//item cost
+		ldi YH, high(item1Cost)	
+		out PORTC, 0b00000001
+		pop temp1
+		rjmp adminMode
+	return2:
+		ldi ZL, low(item2)   
+		ldi ZH, high(item2)
+		ldi YL, low(item2Cost) 	//item cost
+		ldi YH, high(item2Cost)
+		out PORTC, 0b00000011
+		pop temp1
+		rjmp adminMode
+	return3:
+		ldi ZL, low(item3)   
+		ldi ZH, high(item3)
+		ldi YL, low(item3Cost) 	//item cost
+		ldi YH, high(item3Cost)
+		out PORTC, 0b00000111
+		pop temp1
+		rjmp adminMode
+	return4:
+		ldi ZL, low(item4)   
+		ldi ZH, high(item4)
+		ldi YL, low(item4Cost) 	//item cost
+		ldi YH, high(item4Cost)
+		out PORTC, 0b00001111
+		pop temp1
+		rjmp adminMode
+	return5:
+		ldi ZL, low(item5)   
+		ldi ZH, high(item5)
+		ldi YL, low(item5Cost) 	//item cost
+		ldi YH, high(item5Cost)
+		out PORTC, 0b00011111
+		pop temp1
+		rjmp adminMode
+	return6:
+		ldi ZL, low(item6)   
+		ldi ZH, high(item6)
+		ldi YL, low(item6Cost) 	//item cost
+		ldi YH, high(item6Cost)
+		out PORTC, 0b00111111
+		pop temp1
+		rjmp adminMode
+	return7:
+		ldi ZL, low(item7)   
+		ldi ZH, high(item7)
+		ldi YL, low(item7Cost) 	//item cost
+		ldi YH, high(item7Cost)
+		out PORTC, 0b01111111
+		pop temp1
+		rjmp adminMode
+	return8:
+		ldi ZL, low(item8)   
+		ldi ZH, high(item8)
+		ldi YL, low(item8Cost) 	//item cost
+		ldi YH, high(item8Cost)
+		out PORTC, 0b11111111
+		pop temp1
+		rjmp adminMode
+	return9:
+		ldi ZL, low(item9)   
+		ldi ZH, high(item9)
+		ldi YL, low(item9Cost) 	//item cost
+		ldi YH, high(item9Cost)
+		pop temp1
+		rjmp adminMode
+
+	
+
+adminMode:
+	//how does the program get here?
+	rjmp returnInventory  //itll get stuck in a loop here.
+
+	do_lcd_command 0b00000001
+	do_lcd_data 'A'
+	do_lcd_data 'd'
+	do_lcd_data 'm'
+	do_lcd_data 'i'
+	do_lcd_data 'n'
+	do_lcd_data ' '
+	do_lcd_data 'm'
+	do_lcd_data 'o'
+	do_lcd_data 'd'
+	do_lcd_data 'e'
+	do_lcd_data ' '
+	do_lcd_data_r temp1 
+
+	do_lcd_command 0b10101000
+	push temp1
+	push temp2
+
+	ld temp1, Z
+	subi temp1,-'0'
+	do_lcd_data_r temp1   //displays inventory of selected item
+	
+	do_lcd_command 0b10110110
+	do_lcd_data '$'		//displays cost of selected item
+	ld temp1, Y
+	do_lcd_data_r temp1 
+
+	pop temp2
+	pop temp1
+	HashLoop displaySelectScreen2
+
+	jmp KeypadLoop   
+
+
 
 	
 	
